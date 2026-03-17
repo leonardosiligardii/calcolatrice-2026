5a19ac3e35520ca12e7c3a12712ca723bf91644d
leosilli <silli.leo06@gmail.com>
2026-03-17 14:34:20 +0100

    copia degli ultimi file

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,9 +1,9 @@
+
 from funzioni.somma import somma
-from test.test_somma import test01, test02
 
 
-# documentazione: https://github.com/Perteghella/2026-calcolatrice 
-# creo anche dei file __init__ in ogni cartella che per delle versioni di python servono per collegare tutti i file tra loro dentro cartelle diverse, sono file nuovi
+
+
 def main():
     print("Benvenuto nella calcolatrice!")
     num1 = float(input("Inserisci il primo numero: "))
@@ -14,6 +14,4 @@ def main():
     print(f"La somma di {num1} e {num2} è: {risultato}")
 
 if __name__ == "__main__":
-    #main()
-    test01()
-    test02()
+    main()
--- test/test_somma.py
+++ test/test_somma.py
@@ -1,14 +1,14 @@
 from funzioni.somma import somma
 
 
-def test01():
+def test_somma_positivi():
     num1 = 1
     num2 = 2
     risultato = somma(num1, num2)
-    print(f"La somma di {num1} e {num2} è: {risultato}")
+    assert risultato == 3, f"Errore: la somma di {num1} e {num2} dovrebbe essere 3, ma è {risultato}"
 
-def test02():
+def test_somma_negativi():
     num1 = 1.0
     num2 = -2
     risultato = somma(num1, num2)
-    print(f"La somma di {num1} e {num2} è: {risultato}")
+    assert risultato == -1.0, f"Errore: la somma di {num1} e {num2} dovrebbe essere -1.0, ma è {risultato}"
