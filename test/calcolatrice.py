9b75912082b0545de9700bd5a861d818886fb1f1
leosilli <silli.leo06@gmail.com>
2026-03-17 12:22:10 +0100

    commit finale

--- test/calcolatrice.py
+++ test/calcolatrice.py
@@ -3,7 +3,7 @@ from test.test_somma import test01, test02
 
 
 # documentazione: https://github.com/Perteghella/2026-calcolatrice 
-
+# creo anche dei file __init__ in ogni cartella che per delle versioni di python servono per collegare tutti i file tra loro dentro cartelle diverse, sono file nuovi
 def main():
     print("Benvenuto nella calcolatrice!")
     num1 = float(input("Inserisci il primo numero: "))
